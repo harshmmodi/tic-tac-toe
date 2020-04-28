9666abd82c145a9f6102e7136e771b3753b4db56
Harsh Manish Modi <modihar@amazon.com>
2020-04-28 15:21:34 +0530

    Updates

---
+++ precache-manifest.c6ccd6e1198849327e74834ba8e9f8e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28f6301d37aa6240fd504c9a05355b26",
+    "url": "/index.html"
+  },
+  {
+    "revision": "89f2122175bab4e6418d",
+    "url": "/static/css/main.d154bad7.chunk.css"
+  },
+  {
+    "revision": "b5e896cca6806c61485a",
+    "url": "/static/js/2.eb88b07c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.eb88b07c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89f2122175bab4e6418d",
+    "url": "/static/js/main.b17daf8f.chunk.js"
+  },
+  {
+    "revision": "699719e06d67871ab6fd",
+    "url": "/static/js/runtime-main.805a59c0.js"
+  }
+]);
